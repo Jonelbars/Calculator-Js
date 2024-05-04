22790f6a812e811942cc5ef72f464bc229708a66
Jonelbars <jonel.barrientos@hcdc.edu.ph>
2024-05-04 22:29:16 +0800

    temporary styles

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function appendToInput(value) {
-    document.getElementById('input').value += value;
+  document.getElementById("input").value += value;
 }
 
 function clearInput() {
-    document.getElementById('input').value = '';
+  document.getElementById("input").value = " ";
 }
 
 function calculate() {
-    var input = document.getElementById('input').value;
-    var result = eval(input);
-    document.getElementById('input').value = result;
+  var input = document.getElementById("input").value;
+  var result = eval(input);
+  document.getElementById("input").value = result;
 }
